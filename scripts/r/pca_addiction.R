c3f1a926be3adbad8f357288da61cfa34bbe3df6
JoseEspinosa <kadomu@gmail.com>
2020-05-19 17:23:55 +0200

    Adding plots by phase

--- scripts/r/pca_addiction.R
+++ scripts/r/pca_addiction.R
@@ -23,12 +23,15 @@ home <- Sys.getenv("HOME")
 source ("./scripts/r/graph_parameters.R")
 
 # Parameter to set plot qualities
-save_plot = FALSE
+# save_plot = FALSE
+save_plot = TRUE
 
 dpi_q <- 100
 size_text_circle <- 5.5
 title_var_loadings <- "\nVariables factor map\n" 
-out_folder <- "./figures/"  
+out_folder <- "./figures/" 
+out_folder <- "./figures/var_behavioral_test/" 
+
 name_out <- "var_behavioral_test"
 extension_img <- ".png"
 # file_path <- "./data/tbl_PCA_all_variables.csv"
@@ -44,13 +47,37 @@ data <- read.csv (file_path,
                   stringsAsFactors = F)
 
 # reinst_annotation
-reinst_annotation <- read.csv ("./data/annot_descriptors_24_04_20.csv", 
-                               dec=",", 
-                               sep=",", 
-                               stringsAsFactors=FALSE)
+# reinst_annotation <- read.csv ("./data/annot_descriptors_24_04_20.csv", 
+#                                dec=",", 
+#                                sep=",", 
+#                                stringsAsFactors=FALSE)
 
 ## Remove variables that all tables have
-data_filtered <- subset (data, select=-c(Mice, Genotype, Food.pellets, Addiction_categorization_LP)) 
+data_filtered <- subset (data, select=-c(Mice, 
+                                         Genotype, 
+                                         Food.pellets, 
+                                         Addiction_categorization_LP,
+                                         Criteria_EP,
+                                         Criteria_MP,
+                                         Criteria_LP)) 
+
+# subset(dt, select = grep("bar|baz", names(dt)))
+phase <- "LP"
+# phase <- "EP"
+# phase <- "MP"
+
+data_filtered <- subset(data, select = grep(phase, names(data)))
+names(data_filtered)
+data_filtered <- subset (data_filtered, select=-c(
+                                         Addiction_categorization_LP,
+                                         # Criteria_EP,
+                                         # Criteria_MP,
+                                         Criteria_LP))
+
+  
+
+out_folder <- paste0 ("./figures/var_behavioral_test_", phase, "/") 
+name_out <- paste0 ("var_behavioral_test_", phase)
 
 # Remove categorical
 # data_filtered <-subset (data, select=-c(Mice, 
@@ -92,6 +119,8 @@ res = PCA (data_filtered, scale.unit=TRUE)
 v_colours <- c("red", "gray", "blue", "lightblue", "magenta", "orange", "darkgreen")
 cb_palette_adapt <- rep(c("#999999", "#009E73", "#0072B2","#E69F00", "#0072B2", "#D55E00", "#CC79A7"), 3)
 
+cb_palette_mice <- c("#E69F00","#D55E00", "#56B4E9", "#009E73")
+
 # Variance of PCs
 var_PC1 <- round (res$eig [1,2], 1)
 var_PC2 <- round (res$eig [2,2], 1)
@@ -105,18 +134,25 @@ var_PC7 <- round (res$eig [7,2], 1)
 # Convert PCA results to data frame
 pca2plot <- as.data.frame (res$ind$coord)
 pca2plot$id <- data$Mice
-head(data)
+# head(data)
 
 # Changes labels of the groups, some mice are categorized as addicted and the rest as NA (non-addicted)
 data$Addiction_categorization_LP[is.na(data$Addiction_categorization_LP)]  <- "N"
-pca2plot$group <- data$Addiction_categorization_LP
+# pca2plot$group <- data$Addiction_categorization_LP
+pca2plot$group <- paste0(data$Addiction_categorization_LP, "_", data$Criteria_LP)
+class(pca2plot$group)
+pca2plot$group[order(pca2plot$group, decreasing=FALSE)]
+
+pca2plot$group <- factor(pca2plot$group, levels = c("N_0", "N_1", "A_2", "A_3"))
 
 ## PC1 vs. PC3
 title_p <- paste ("PCA addiction, PC1 vs. PC2\n", sep="")
 
 pca_addiction_PC1_PC2 <- ggplot (pca2plot, aes(x=Dim.1, y=Dim.2, colour=group)) + 
                  geom_point (size = 3.5, show.legend = T) + 
-                 scale_color_manual(values=c("orange", "blue")) +
+                 # scale_color_manual(values=c("orange", "blue")) +
+                 # scale_color_manual (values=cb_palette_adapt) +
+                 scale_color_manual (values=cb_palette_mice) +
                  geom_text (aes(label=id), vjust=-0.5, hjust=1, size=4, show.legend = F)+
                  theme(legend.key=element_rect(fill=NA)) +
                  scale_x_continuous (limits=c(-10, 12), breaks=-10:12) + 
@@ -142,7 +178,9 @@ title_p <- paste ("PCA addiction, PC1 vs. PC3\n", sep="")
 
 pca_addiction_PC1_PC3 <- ggplot (pca2plot, aes(x=Dim.1, y=Dim.3, colour=group)) + 
   geom_point (size = 3.5, show.legend = T) + 
-  scale_color_manual(values=c("orange", "blue")) +
+  # scale_color_manual(values=c("orange", "blue")) +
+  # scale_color_manual (values=cb_palette_adapt) +
+  scale_color_manual (values=cb_palette_mice) +
   geom_text (aes(label=id), vjust=-0.5, hjust=1, size=4, show.legend = F)+
   theme(legend.key=element_rect(fill=NA)) +
   scale_x_continuous (limits=c(-10, 12), breaks=-10:12) + 
@@ -168,7 +206,9 @@ title_p <- paste ("PCA addiction, PC2 vs. PC3\n", sep="")
 
 pca_addiction_PC2_PC3 <- ggplot (pca2plot, aes(x=Dim.2, y=Dim.3, colour=group)) + 
   geom_point (size = 3.5, show.legend = T) + 
-  scale_color_manual(values=c("orange", "blue")) +
+  # scale_color_manual(values=c("orange", "blue")) +
+  # scale_color_manual (values=cb_palette_adapt) +
+  scale_color_manual (values=cb_palette_mice) +
   geom_text (aes(label=id), vjust=-0.5, hjust=1, size=4, show.legend = F)+
   theme(legend.key=element_rect(fill=NA)) +
   scale_x_continuous (limits=c(-10, 12), breaks=-10:12) + 
@@ -412,8 +452,6 @@ circle_plot_by_gr <- function (data_annotation, pc_x="Dim.1", pc_y="Dim.2", grou
 }
 
 circle_plot_pc1_pc2 <- circle_plot_by_gr(circle_plot_annotation_merged)
-class (circle_plot_annotation_merged$Variable)
-circle_plot_pc1_pc2
 circle_plot_pc1_pc3 <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.1", pc_y="Dim.3")
 circle_plot_pc2_pc3 <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.2", pc_y="Dim.3")
 
@@ -421,22 +459,64 @@ circle_plot_by_behavior_pc1_pc2 <- circle_plot_by_gr(circle_plot_annotation_merg
 circle_plot_by_behavior_pc1_pc3 <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.1", pc_y="Dim.3", grouping_var="Annotation")
 circle_plot_by_behavior_pc2_pc3 <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.2", pc_y="Dim.3", grouping_var="Annotation")
 
-circle_plot_by_behavior_pc1_pc2_labels <- circle_plot_by_gr(circle_plot_annotation_merged, grouping_var="Annotation", gr_plot_type="labels")
-circle_plot_by_behavior_pc1_pc3_labels <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.1", pc_y="Dim.3", grouping_var="Annotation", gr_plot_type="labels")
-circle_plot_by_behavior_pc2_pc3_labels <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.2", pc_y="Dim.3", grouping_var="Annotation", gr_plot_type="labels")
+# circle_plot_by_behavior_pc1_pc2_labels <- circle_plot_by_gr(circle_plot_annotation_merged, grouping_var="Annotation", gr_plot_type="labels")
+# circle_plot_by_behavior_pc1_pc3_labels <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.1", pc_y="Dim.3", grouping_var="Annotation", gr_plot_type="labels")
+# circle_plot_by_behavior_pc2_pc3_labels <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.2", pc_y="Dim.3", grouping_var="Annotation", gr_plot_type="labels")
 
 circle_plot_by_period_pc1_pc2 <- circle_plot_by_gr(circle_plot_annotation_merged, grouping_var="Period")
 circle_plot_by_period_pc1_pc3 <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.1", pc_y="Dim.3", grouping_var="Period")
 circle_plot_by_period_pc2_pc3 <- circle_plot_by_gr(circle_plot_annotation_merged, pc_x="Dim.2", pc_y="Dim.3", grouping_var="Period")
 
 # plot_name <- "PCA_factors_addiction"
-plot_name <- "PCA_factors_addiction_1st_analysis_PCA_selection_variables"
+# plot_name <- "PCA_factors_addiction_1st_analysis_PCA_selection_variables"
+
 if (save_plot) {
-  ggsave (p_circle_points_coord_fixed, 
-          file=paste('/home/kadomu/projects/20200421_pca_behavior_elena/results/',
-                                                  plot_name, extension_img, sep=""), width = 15, height = 15, dpi=dpi_q)
+  ggsave (circle_plot_pc1_pc2,
+          file=paste(out_folder, name_out, "_circle_PC1_vs_PC2", extension_img, sep=""), 
+          width = 15, height = 15, dpi=dpi_q)
 } else {
-  p_circle_points_coord_fixed
+  circle_plot_pc1_pc2
+}
+
+if (save_plot) {
+  ggsave (circle_plot_pc2_pc3,
+          file=paste(out_folder, name_out, "_circle_PC2_vs_PC3", extension_img, sep=""), 
+          width = 15, height = 15, dpi=dpi_q)
+} else {
+  circle_plot_pc2_pc3
+}
+
+if (save_plot) {
+  ggsave (circle_plot_pc1_pc3,
+          file=paste(out_folder, name_out, "_circle_PC1_vs_PC3", extension_img, sep=""), 
+          width = 15, height = 15, dpi=dpi_q)
+} else {
+  circle_plot_pc1_pc3
+}
+
+######## AQUI
+if (save_plot) {
+  ggsave (circle_plot_by_period_pc1_pc2,
+          file=paste(out_folder, name_out, "_by_period_circle_PC1_vs_PC2", extension_img, sep=""), 
+          width = 15, height = 15, dpi=dpi_q)
+} else {
+  circle_plot_by_behavior_pc1_pc2
+}
+
+if (save_plot) {
+  ggsave (circle_plot_pc2_pc3,
+          file=paste(out_folder, name_out, "_circle_PC2_vs_PC3", extension_img, sep=""), 
+          width = 15, height = 15, dpi=dpi_q)
+} else {
+  circle_plot_pc2_pc3
+}
+
+if (save_plot) {
+  ggsave (circle_plot_pc1_pc3,
+          file=paste(out_folder, name_out, "_circle_PC1_vs_PC3", extension_img, sep=""), 
+          width = 15, height = 15, dpi=dpi_q)
+} else {
+  circle_plot_pc1_pc3
 }
 
 ############
@@ -478,25 +558,31 @@ bars_plot_pc2
 bars_plot_pc3 
 
 if (save_plot) {
-  ggsave (bars_plot_pc1, file=paste(home, "/old_data/figures/", 
-                                "bars_PC1_",  "impulsivity.tiff", sep=""), width = 15, height = 12, dpi=dpi_q)
+  ggsave (bars_plot_pc1,
+          file=paste(out_folder, name_out, "_bars_PC1", extension_img, sep=""), 
+          width = 15, height = 12, dpi=dpi_q)
 } else {
   bars_plot_pc1
 }
 
 if (save_plot) {
-  ggsave (bars_plot_PC2, file=paste(home, "/old_data/figures/", 
-                                    "bars_PC2_",  phase, "Phase.tiff", sep=""), width = 15, height = 12, dpi=dpi_q)
+  ggsave (bars_plot_pc2,
+          file=paste(out_folder, name_out, "_bars_PC2", extension_img, sep=""), 
+          width = 15, height = 12, dpi=dpi_q)
 } else {
   bars_plot_pc2
 }
 
 if (save_plot) {
-  ggsave (bars_plot_pc3, file=paste(home, "/old_data/figures/", 
-                                    "bars_PC3_",  phase, "Phase.tiff", sep=""), width = 15, height = 12, dpi=dpi_q)
+  ggsave (bars_plot_pc3,
+          file=paste(out_folder, name_out, "_bars_PC3", extension_img, sep=""), 
+          width = 15, height = 12, dpi=dpi_q)
 } else {
   bars_plot_pc3
 }
+
+
+
 
 
 
@@ -529,139 +615,4 @@ dailyInt_theme <- theme_update (axis.title.x = element_text (size=base_size * 2,
 
 
 
-# Plotting the variables by experimental phase
-circle_plot$var <- rownames (circle_plot)
-
-circle_plot$var <- gsub ("day", "", circle_plot$var)
-circle_plot$var <- gsub ("dep_IMUL", "dep", circle_plot$var)
-circle_plot$var <- gsub ("adlib_IMPUL", "adlib" , circle_plot$var)
-
-circle_plot$varGroup <- circle_plot$var
-circle_plot$varGroup [grep("^dep", circle_plot$var)] <- "dep_impul"
-circle_plot$varGroup [grep("^adlib", circle_plot$var)] <- "ad_impul"
-
-# circle_plot$varGroup [grep("^adlib_act", circle_plot$var)] <- "adlib_act"
-# circle_plot$varGroup [grep("^adlib_inact", circle_plot$var)] <- "adlib_in"
-# circle_plot$varGroup [grep("^ex_act", circle_plot$var)] <- "ex_act"
-# circle_plot$varGroup [grep("^ex_inact", circle_plot$var)] <- "ex_inact"
-# circle_plot$varGroup [c(81:length(circle_plot$varGroup))] <- "others"
-as.factor(circle_plot$varGroup)
-colnames (circle_plot) <- c("Dim.1", "Dim.2", "Dim.3", "Dim.4", "Dim.5", "var", "varGroup")
-
-# I only need the nummber of session for each of them
-circle_plot$session <- gsub("^dep_", "", circle_plot$var) 
-circle_plot$session <- gsub("^adlib_", "", circle_plot$session)
-
-# circle_plot$session <- gsub("^adlib_inact_", "", circle_plot$session)
-# circle_plot$session <- gsub("^ex_act_", "", circle_plot$session)
-# circle_plot$session <- gsub("^ex_inact_", "", circle_plot$session)
-
-############
-# Doing a circle plot with arrows coloured by experimental phase
-
-# Adding session to the circle_plot df to plot them
-neg_labels <- labels_v [which (circle_plot$Dim.1 < 0)]
-neg_positions <- circle_plot [which (circle_plot$Dim.1 < 0), c(1,2,8)]
-
-pos_labels <- labels_v [which (circle_plot$Dim.1 >= 0)]
-pos_positions <- circle_plot [which (circle_plot$Dim.1 >= 0), c(1,2,8)]
-
-title_c <- paste ("PCA of the variables ","- presses during time-out\n", sep="")
-
-p_circle_plot_colors <- ggplot(circle_plot) + 
-  geom_segment (data=circle_plot, aes(colour=varGroup, x=0, y=0, xend=Dim.1, yend=Dim.2), 
-                arrow=arrow(length=unit(0.2,"cm")), alpha=1, size=1) +
-  scale_color_manual (values =  cb_palette_adapt) +
-  xlim (c(-1.2, 1.2)) + ylim (c(-1.2, 1.2)) +
-  geom_text (data=neg_positions, aes (x=Dim.1, y=Dim.2, label=session, hjust=0.9, vjust=-0.4), 
-             show.legend = FALSE, size=5) + 
-  geom_text (data=pos_positions, aes (x=Dim.1, y=Dim.2, label=session, hjust=-0.2), 
-             show.legend = FALSE, size=5) +
-  geom_vline (xintercept = 0, linetype="dotted") +
-  geom_hline (yintercept=0, linetype="dotted") +
-  labs (title = title_c, x = paste("\nPC1 (", var_PC1, "% of variance)", sep=""), 
-        y=paste("PC2 (", var_PC2, "% of variance)\n", sep = "")) +
-  geom_polygon (data = df.circle, aes(x, y), alpha=1, colour="black", fill=NA, size=1) +
-  theme (legend.key = element_blank(), legend.key.height = unit (1.5, "line"), 
-         legend.title=element_blank()) 
-base_size <- 10
-
-p_circle_plot_colors
-dailyInt_theme <- theme_update (axis.title.x = element_text (size=base_size * 2, face="bold"),
-                                axis.title.y = element_text (size=base_size * 2, angle = 90, face="bold"),
-                                plot.title = element_text (size=base_size * 2, face="bold"))
-p_circle_plot_colors
-
-if (save_plot) {
-  ggsave (p_circle_plot_colors, 
-          file=paste(home, "/old_data/figures/", 
-                     "circle_color_act_",  "impulsivity.tiff", sep=""), width = 15, height = 12, dpi=dpi_q)
-}
-
-# # Colour circle plot by session 1-5 5-10 10-15 15-20
-# circle_plot 
-# circle_plot$session_bin <- "" 
-# 
-# circle_plot [which (as.numeric (circle_plot$session) < 6), "session_bin"] <- "1_5"
-# circle_plot [which (as.numeric (circle_plot$session) > 5 & as.numeric (circle_plot$session)< 11), "session_bin"] <- "6_10"
-# circle_plot [which (as.numeric (circle_plot$session) > 10 & as.numeric (circle_plot$session)< 16), "session_bin"] <- "11_15"
-# circle_plot [which (as.numeric (circle_plot$session) > 15), "session_bin"] <- "16_20"
-# 
-# # Plot with arrows coloured by session bin
-# p_circle_plot_colors_bin <- ggplot(circle_plot) + 
-#   geom_segment (data=circle_plot, aes(colour=session_bin, x=0, y=0, xend=Dim.1, yend=Dim.2), 
-#                 arrow=arrow(length=unit(0.2,"cm")), alpha=1, size=1) +
-#   scale_color_manual (values = v_colours) +
-#   xlim (c(-1.2, 1.2)) + ylim (c(-1.2, 1.2)) +
-#   geom_text (data=neg_positions, aes (x=Dim.1, y=Dim.2, label=session, hjust=0.9, vjust=-0.4), 
-#              show.legend = FALSE, size=5) + 
-#   geom_text (data=pos_positions, aes (x=Dim.1, y=Dim.2, label=session, hjust=-0.2), 
-#              show.legend = FALSE, size=5) +
-#   geom_vline (xintercept = 0, linetype="dotted") +
-#   geom_hline (yintercept=0, linetype="dotted") +
-#   labs (title = title_c, x = paste("\nPC1 (", var_PC1, "% of variance)", sep=""), 
-#         y=paste("PC2 (", var_PC2, "% of variance)\n", sep = "")) +
-#   geom_polygon (data = df.circle, aes(x, y), alpha=1, colour="black", fill=NA, size=1) +
-#   theme (legend.key = element_blank(), legend.key.height = unit (1.5, "line"), 
-#          legend.title=element_blank()) 
-# 
-# base_size <- 10
-# 
-# dailyInt_theme <- theme_update (axis.title.x = element_text (size=base_size * 2, face="bold"),
-#                                 axis.title.y = element_text (size=base_size * 2, angle = 90, face="bold"),
-#                                 plot.title = element_text (size=base_size * 2, face="bold"))
-# p_circle_plot_colors_bin
-
-# ggsave (p_circle_plot_colors_bin, file=paste(home, "/old_data/figures/", 
-#                                              "circle_color_bin_",  phase, "Phase.tiff", sep=""), width = 15, height = 12, dpi=dpi_q)
-
-# Doing the same plot as above by colours but in this case facet
-p_var_by_group_scale_free <- ggplot(circle_plot) + 
-  labs (title = title_c, x = paste("PC1 (", var_PC1, "% of variance)", sep=""), 
-        y=paste("PC2 (", var_PC2, "% of variance)", sep = "")) +
-  geom_text (aes(colour=varGroup, x=Dim.1, y=Dim.2, label=session), show.legend = FALSE, size=5) +
-  scale_color_manual (values = c("red", "gray", "blue", "lightblue", "magenta", "orange", "darkgreen")) +
-  facet_wrap(~varGroup, scales="free")
-
-p_var_by_group_scale_free
-# ggsave (p_var_by_group_scale_free, file=paste(home, "/old_data/figures/", 
-#                                    "points_act_facet_",  phase, "Phase.tiff", sep=""), width = 15, height = 12, dpi=dpi_q)
-
-p_var_by_group <- ggplot(circle_plot) + 
-  xlim (c(-1, 1)) + ylim (c(-1, 1)) +
-  geom_text (aes(colour=varGroup, x=Dim.1, y=Dim.2, label=session), show.legend = FALSE, size=5, vjust=-0.4) +
-  geom_point(aes(colour=varGroup, x=Dim.1, y=Dim.2), size=3)+
-  scale_color_manual (values = v_colours) +
-  labs (title = title_c)
-labs (x = paste("\nPC1 (", var_PC1, "% of variance)", sep=""), 
-      y=paste("PC2 (", var_PC2, "% of variance)\n", sep = "")) +
-  theme (legend.key = element_blank(), legend.key.height = unit (1.5, "line"), 
-         legend.title=element_blank()) 
-
-p_var_by_group
-
-if (save_plot) {
-  # ggsave (p_var_by_group, file=paste(home, "/old_data/figures/", 
-  #                                          "points_act_",  phase, "Phase.tiff", sep=""), width = 15, height = 12, dpi=dpi_q)
-}
 
