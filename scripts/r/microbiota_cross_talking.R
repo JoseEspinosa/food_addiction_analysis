8cc7e43b8f8d839c02a4de2c9a4830fe84fdacf1
JoseEspinosa <kadomu@gmail.com>
2020-10-01 14:33:05 +0200

    Improve how it deals with the different taxon tbl

--- scripts/r/microbiota_cross_talking.R
+++ scripts/r/microbiota_cross_talking.R
@@ -24,17 +24,16 @@ transpose_df <- function(df) {
 }
 
 ### microbiota data
-taxon <- "phylum"
-taxon <- "family"
-taxon <- "genus"
+taxon <- "phylum"; sep_f="\t"
+# taxon <- "family"; sep_f=";"
+# taxon <- "genus"; sep_f=";"
 
 home_dir <- Sys.getenv("HOME")
 rel_abundance_by_taxon <- paste0(home_dir, "/git/food_addiction_analysis/data/microbiota/relative_abundances_by_", taxon, ".csv")
 # rel_abundance_by_phylum <- "/Users/jaespinosa/git/food_addiction_analysis/data/microbiota/relative_abundances_by_phylum.csv"
 microbiota_by_phylum_ori <- read.csv(rel_abundance_by_taxon,
                                      dec=",",
-                                     sep=";",
-                                     # sep="\t",
+                                     sep=sep_f,
                                      check.names = F,
                                      stringsAsFactors = F)
 
@@ -46,10 +45,13 @@ microbiota_by_phylum <- read.csv(paste0(home_dir, "/tmp.csv"),
                                  stringsAsFactors = F)
 head(microbiota_by_phylum)
 
-## Behavioral data
-behavioral_data_path <- paste0(home_dir,
-                               "/git/food_addiction_analysis/data/microbiota/behavioral_data_from_results_microbiota_16_04_20_withoutCategorical.csv")
+## Behavioral data all variables
+# behavioral_data_path <- paste0(home_dir,
+#                                "/git/food_addiction_analysis/data/microbiota/behavioral_data_from_results_microbiota_16_04_20_withoutCategorical.csv")
 
+# Only behavioral variables used in PCA
+behavioral_data_path <- paste0(home_dir,
+                               "/git/food_addiction_analysis/data/microbiota//behavioral_data_from_results_microbiota_16_04_20_PCA_variables.csv")
 behavioral_data <- read.csv(behavioral_data_path,
                             dec=",",
                             sep=";",
@@ -74,22 +76,24 @@ microbio_behavioral_merged <- merge (microbiota_relAbund, behavioral_cont_data, 
 data <- gather(microbio_behavioral_merged, taxon, microbio_rel_ab, Verrucomicrobia:Actinobacteria)%>%
                gather(behavior_idx, index, Persistence_EP:Acquisition_stability)
 ## family
-data <- gather(microbio_behavioral_merged, taxon, microbio_rel_ab, Alcaligenaceae:Others)%>%
-               gather(behavior_idx, index, Persistence_EP:Acquisition_stability)
-
-## genus
-data <- gather(microbio_behavioral_merged, taxon, microbio_rel_ab, Acetatifactor:Tyzzerella)%>%
-               gather(behavior_idx, index, Persistence_EP:Acquisition_stability)
-
+# data <- gather(microbio_behavioral_merged, taxon, microbio_rel_ab, Alcaligenaceae:Others)%>%
+#                gather(behavior_idx, index, Persistence_EP:Acquisition_stability)
+# 
+# ## genus
+# data <- gather(microbio_behavioral_merged, taxon, microbio_rel_ab, Acetatifactor:Tyzzerella)%>%
+#                gather(behavior_idx, index, Persistence_EP:Acquisition_stability)
+# 
 
 # head(data)
 data_nest <- group_by(data, taxon, behavior_idx) %>% nest()
 # head(data_nest)
 
 # data_nest <- group_by(data, city, telecon) %>% nest()
-cor_fun <- function(df) cor.test(df$microbio_rel_ab, df$index, method = "spearman") %>% tidy()
+# cor_method <- "pearson"
+cor_method <- "spearman"
+cor_fun <- function(df) cor.test(df$microbio_rel_ab, df$index, method = cor_method) %>% tidy()
 
-# library(broom)
+library(broom) # Convert results of statistical functions (lm, t.test, cor.test, etc.) into tidy tables
 
 # library(fs)
 # library(lubridate)
@@ -135,6 +139,18 @@ dpi_q <- 200
 extension_img <- ".png"
 ggsave (hm, file=paste0(out_dir, "heatmap_", taxon, extension_img), 
         width = 20, height = 12, dpi=dpi_q)
+
+
+cor.test(microbio_behavioral_merged$Aversive_MP, microbio_behavioral_merged$Deferribacteres,
+                method = "spearman")
+
+ggplot (microbio_behavioral_merged, aes(x=Aversive_MP, y=Deferribacteres)) + 
+        geom_point() +
+        theme_minimal()+
+        theme(panel.grid.major = element_blank(),
+              panel.border = element_blank(),
+              panel.background = element_blank(),
+              axis.ticks = element_blank())
 
 ##########################
 ################
