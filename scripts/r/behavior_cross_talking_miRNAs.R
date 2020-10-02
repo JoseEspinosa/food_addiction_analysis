3e5ef7a78d7b1fde161e29aaa00287ba74c56337
JoseEspinosa <kadomu@gmail.com>
2020-10-02 10:26:06 +0200

    Adding cross talking of beh PCA vars vs. miRNAs

---
+++ scripts/r/behavior_cross_talking_miRNAs.R
@@ -0,0 +1,155 @@
+#!/usr/bin/env Rscript
+
+#############################################################################
+### Jose Espinosa-Carrasco CB-CRG Group. Sep 2020                         ###
+#############################################################################
+### Cross-talking between behavior and microbiota                         ###
+#############################################################################
+
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+
+## Functions
+transpose_df <- function(df) {
+  # keep the first column 
+  names <-  df[,1]
+  
+  # Transpose everything other than the first column
+  df.T <- as.data.frame(as.matrix(t(df[,-1])))
+  
+  # Assign first column as the column names of the transposed dataframe
+  colnames(df.T) <- names
+  return(df.T)
+}
+
+# Only behavioral variables used in PCA
+behavioral_data_path <- paste0(home_dir,
+                               "/git/food_addiction_analysis/data/microbiota//behavioral_data_from_results_microbiota_16_04_20_PCA_variables.csv")
+behavioral_data <- read.csv(behavioral_data_path,
+                            dec=",",
+                            sep=";",
+                            check.names = F,
+                            stringsAsFactors = F)
+# head(behavioral_data)
+
+# microRNAs data
+# discovery
+miRNAs_data_path_discovery <- paste0(home_dir,
+                                     "/git/food_addiction_analysis/data/microbiota/miRNAs/resultats_miRNAs_FA_14.04.20.csv")
+
+miRNAs_data_discovery <- read.csv(miRNAs_data_path_discovery,
+                                  dec=",",
+                                  sep=";",
+                                  check.names = F,
+                                  stringsAsFactors = F)
+
+miRNAs_data_path_replica <- paste0(home_dir,
+                                   "/git/food_addiction_analysis/data/microbiota/miRNAs/resultats_miRNAs_FA_14.04.20_replica.csv")
+
+miRNAs_data_replica <- read.csv(miRNAs_data_path_replica,
+                                dec=",",
+                                sep=";",
+                                check.names = F,
+                                stringsAsFactors = F)
+
+miRNAs_data_to_transp <- merge (miRNAs_data_replica, miRNAs_data_discovery, by="miRNA")
+
+miRNAs_data <- transpose_df(miRNAs_data_to_transp)
+miRNAs_data$mouse_id <- row.names(miRNAs_data)
+
+## Select miRNAs from Elena's table
+miRNAs_data_selected <- subset(miRNAs_data, select=c('mouse_id',
+                                                     'mmu-miR-876-5p',
+                                                     'mmu-miR-211-5p',
+                                                     'mmu-miR-3085-3p',
+                                                     'mmu-miR-665-3p',
+                                                     'mmu-miR-3072-3p',
+                                                     'mmu-miR-124-3p',
+                                                     'mmu-miR-29c-3p',
+                                                     'mmu-miR-544-3p',
+                                                     'mmu-miR-137-3p',
+                                                     'mmu-miR-100-5p',
+                                                     'mmu-miR-192-5p'))
+
+miRNAs_behavioral_merged <- merge (miRNAs_data_selected, behavioral_data, by= "mouse_id")
+head(miRNAs_behavioral_merged)
+
+## All miRNAs
+
+## selected miRNAs
+data <- gather(miRNAs_behavioral_merged, miRNA, value, 'mmu-miR-876-5p':'mmu-miR-192-5p')%>%
+  gather(behavior_idx, index, Persistence_LP:Aversive_LP)
+
+
+data_nest <- group_by(data, miRNA, behavior_idx) %>% nest()
+
+
+# data_nest <- group_by(data, city, telecon) %>% nest()
+# cor_method <- "pearson"
+cor_method <- "spearman"
+cor_fun <- function(df) cor.test(df$value, df$index, method = cor_method) %>% tidy()
+
+library(broom) # Convert results of statistical functions (lm, t.test, cor.test, etc.) into tidy tables
+
+# library(fs)
+# library(lubridate)
+
+data_nest <- mutate(data_nest, model = map(data, cor_fun))
+data_nest
+
+# str(slice(data_nest, 1))
+
+corr_pr <- select(data_nest, -data) %>% unnest()
+corr_pr <- mutate(corr_pr, sig = ifelse(p.value <0.05, "Sig.", "Non Sig."))
+
+hm <- ggplot() + geom_tile(data = corr_pr,
+                           # aes(behavior_idx, miRNA, fill = estimate),
+                           aes(miRNA, behavior_idx, fill = estimate),
+                               size = 1,
+                               colour = "white") +
+      ## black lines around tiles
+      # geom_tile(data = filter(corr_pr, sig == "Sig."),
+      #           # aes(behavior_idx, miRNA),
+      #           aes(miRNA, behavior_idx),
+      #           size = 1,
+      #           colour = "black",
+      #           fill = "transparent") +
+      geom_text(data = corr_pr, 
+                # angle = 270, 
+                size=5,
+                # aes(behavior_idx, miRNA, #label = round(estimate, 2),
+                aes(miRNA, behavior_idx,    
+                    label = ifelse(sig == "Sig.", round(estimate, 2), ""))) +
+                    # fontface = ifelse(sig == "Sig.", "bold", "plain"))) +
+          # scale_fill_gradient2(breaks = seq(-1, 1, 0.2)) +
+      scale_fill_gradient2(breaks = seq(-1, 1, 0.2), #midpoint = mid, 
+                           low = "#d53e4f", mid = "white",
+                           high = "#abdda4") + 
+      labs(y = "", x = "", fill = "", p.value = "") +
+      theme_minimal() +
+      theme(panel.grid.major = element_blank(),
+            panel.border = element_blank(),
+            panel.background = element_blank(),
+            axis.ticks = element_blank(),
+            axis.text = element_text(size=16),
+            axis.text.x = element_text(angle = 90),
+            legend.text = element_text( size=14))
+
+out_dir <- paste0(home_dir, "/git/food_addiction_analysis/figures/cross_talking_behavior_miRNAs/")
+dpi_q <- 200
+extension_img <- ".png"
+ggsave (hm, file=paste0(out_dir, "heatmap_fewVar_", "miRNAs", extension_img), 
+        width = 20, height = 12, dpi=dpi_q)
+
+
+cor.test(microbio_behavioral_merged$Aversive_MP, microbio_behavioral_merged$Deferribacteres,
+         method = "spearman")
+
+ggplot (microbio_behavioral_merged, aes(x=Aversive_MP, y=Deferribacteres)) + 
+  geom_point() +
+  theme_minimal()+
+  theme(panel.grid.major = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.ticks = element_blank())
