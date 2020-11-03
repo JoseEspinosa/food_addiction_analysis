0e9b3097ed080ac3e0cd3207c7f03b2d0fbe7bc1
JoseEspinosa <kadomu@gmail.com>
2020-11-03 21:04:04 +0100

    Adding distance correlations with filtered data

--- scripts/r/distance_correlations.R
+++ scripts/r/distance_correlations.R
@@ -10,15 +10,15 @@ taxon <- "genus"
 ra <- paste0(home_dir, "/git/food_addiction_analysis/forJose/relative_abundances_by_", taxon, ".txt")
 ma <- read.table(ra, sep="\t", skip=1,
                  row.names=1, colClasses = "character")
-ma
+
 m_microbiome=read.table(ra,sep="\t",skip=3,row.names=1)
-m
-colnames(m)=ma[1,]
-m
-cor_m_microbiome <- cor(m, method="pearson")
+colnames(m_microbiome)=ma[1,]
+
+cor_m_microbiome <- cor(m_microbiome, method="pearson")
 cor_m
 dist_m_microbiome <- 1-cor_m
 
+##############
 ## Behavior
 bf <- paste0(home_dir, "/git/food_addiction_analysis/forJose/behavior.txt")
 mb=read.table(bf,header=TRUE,sep="\t",row.names=1)
@@ -37,16 +37,33 @@ str(dist_m_beh)
 str(dist_m_microbiome)
 
 cor(c(dist_m_beh), c(dist_m_beh))
-cor(c(dist_m_beh), c(dist_m_microbiome), method="spearman")
+cor(c(dist_m_beh), c(dist_m_microbiome), method="pearson")
+
+
+#########################################
+# Filter microbiome abundances like Ionas
+M=t(m_microbiome)
+rownames(M)=ma[1,]
+
+## Any genera with zeros is removed
+test=apply(M,2,min)
+test
+M_f = M[,-which(test==0)]
+mb_t_f_ord <- t(M_f) [, col_names_microb] 
+mb_t_f_ord
+
+cor_m_microbiome_filt <- cor(mb_t_f_ord, method="pearson")
+cor_m_microbiome_filt
+dist_m_microbiome_filt <- 1-cor_m_microbiome_filt
+
+cor(c(dist_m_beh), c(dist_m_microbiome_filt), method="pearson")
+length(c(dist_m_beh))
+length(c(dist_m_microbiome_filt))
+colnames (dist_m_beh)
+colnames (dist_m_microbiome_filt)
 
 
 
-
-
-
-MB=mb[rownames(M2),]
-
-?cor
 
 
 # install.packages("tidyverse")
